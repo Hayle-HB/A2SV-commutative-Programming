cd5c21dcb9f0ada4f48a5ee2693932a36e491773
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-05-07 14:17:14 +0300

    Time: 224 ms (78.25%) | Memory: 20.5 MB (23.94%) - LeetSync

---
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -0,0 +1,13 @@
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def func(head):
+            a=head.val*2
+            if head.next:
+                a+=func(head.next)
+            head.val=a%10
+            return a//10
+        a=func(head)
+        if a:
+            return ListNode(a,head)
+        return head
+        
