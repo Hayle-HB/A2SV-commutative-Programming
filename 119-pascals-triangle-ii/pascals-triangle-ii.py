6e472756d17385695ed7f0992f77a7e11e770596
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-05-17 18:09:12 +0300

    Time: 41 ms (16.90%) | Memory: 16.5 MB (38.30%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,12 +1,19 @@
-class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex == 0:
-            return [1] 
-        
-        prev = self.getRow(rowIndex-1)
-        row = [1]
 
-        for i in range(1, len(prev)):
-            row.append(prev[i-1] + prev[i])
-        row.append(1)
-        return row
+""""
+             1
+           1   1
+          1   2   1
+        1   3   3   1
+       1  4   6   4   1
+
+"""
+class Solution(object):
+    def getRow(self, rowIndex):
+        result = [1] * (rowIndex + 1)
+
+        for i in range(1, rowIndex + 1):
+            val = result[i - 1] * (rowIndex - i + 1) / i
+            result[i] = int(val)
+
+        return result
+
