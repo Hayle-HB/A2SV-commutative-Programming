404cecf1513b45b99091cb7351e524139c67a0d4
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-06-25 11:46:31 +0300

    Time: 50 ms (25.66%) | Memory: 18.1 MB (8.13%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -1,21 +1,34 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left = None
+        self.right = right = None
+
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
         if not root:
             return ""
-        result = str(root.val)
-
-        if root.left or root.right:
-            result += f"({self.tree2str(root.left)})"
-        if root.right:
-            result += f"({self.tree2str(root.right)})"
         
-        return result
+        stack = [root]
+        visited = set()
+        ans = []
 
+        while stack:
+            node = stack[-1]
+            
+            if node in visited:
+                stack.pop()
+                ans.append(")")
+            else:
+                visited.add(node)
+                ans.append("(" + str(node.val))
+                
+                if node.left is None and node.right is not None:
+                    ans.append("()")
+                
+                if node.right:
+                    stack.append(node.right)
+                if node.left:
+                    stack.append(node.left)
 
-        
+        return "".join(ans)[1:-1]
