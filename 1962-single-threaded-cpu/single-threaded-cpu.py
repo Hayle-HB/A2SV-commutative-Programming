a8fc2cc2aa8b2acb02c4aff4ff75e0a75cd84b59
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-06-26 18:55:20 +0300

    Time: 1391 ms (87.55%) | Memory: 65.8 MB (28.67%) - LeetSync

---
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -0,0 +1,28 @@
+import heapq
+
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        for i in range(len(tasks)):
+            tasks[i].append(i)
+        
+        tasks.sort()
+        
+        result = []
+        min_heap = []
+        time = 0
+        i = 0
+        n = len(tasks)
+        
+        while i < n or min_heap:
+            if not min_heap and time < tasks[i][0]:
+                time = tasks[i][0]
+            
+            while i < n and tasks[i][0] <= time:
+                heapq.heappush(min_heap, (tasks[i][1], tasks[i][2]))
+                i += 1
+            
+            process_time, index = heapq.heappop(min_heap)
+            time += process_time
+            result.append(index)
+        
+        return result
