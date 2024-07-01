547a3ff9165152b612a7fb82ea59a41d74363a70
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-07-01 18:34:54 +0300

    Time: 37 ms (99.97%) | Memory: 23.1 MB (90.85%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        count = [0] * (n + 1)
+
+        for i in range(n+1):
+            count[i] = count[i>>1] + (i & 1)
+        return count
+        
+                
+
