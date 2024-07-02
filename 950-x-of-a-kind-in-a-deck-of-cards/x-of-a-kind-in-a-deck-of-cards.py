1293acc1c5772d7d7ae5e9d5b4226ef8577cc4ee
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-07-02 16:10:05 +0300

    Update x-of-a-kind-in-a-deck-of-cards.py

--- 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
@@ -6,4 +6,24 @@ class Solution:
         g = reduce(gcd, counter.values())
 
         return g >= 2
+"""
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        def gcd(a, b):
+            if b == 0:
+                return a 
+            return gcd(b, a % b)
+        if len(deck) <= 1:
+            return False
+
+        count = Counter(deck)
+        expect = min(count.values())
+
+        for num in count.values():
+            expect = gcd(num, expect)
+            if expect == 1:
+                return False
+        return True
+        
+"""
         
