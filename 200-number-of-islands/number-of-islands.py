4acd504328022bd99f963b6d41636b01cbbfda86
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-07-07 04:24:43 +0300

    Time: 417 ms (5.03%) | Memory: 33.8 MB (5.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,64 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        direction = [
+        if not grid:
+            return 0
+
+        n = len(grid)
+        m = len(grid[0])
+
+
+        parent = {i*m + j: i * m + j for i in range(n) for j in range(m)}
+        rank = [1] * (n * m)
+
+
+        def find(i):
+            if parent[i] == i:
+                return parent[i]
+            else:
+                parent[i] = find(parent[i])
+            
+            return parent[i]
+
+        def union(i, j):
+            pi = find(i)
+            pj = find(j)
+
+            if rank[pi] > rank[pj]:
+                parent[pj] = pi
+            elif rank[pj] > rank[pi]:
+                parent[pi] =pj
+            else:
+                parent[pj] = pi
+                rank[pi] += 1
+
+        dxn = [
             (1, 0),
             (-1, 0),
             (0, 1),
             (0, -1)
         ]
-        n = len(grid)
-        m = len(grid[0])
 
-        def dfs(r, c):
-            if not (0 <= r < n  and 0 <= c < m and grid[r][c] == '1'):
-                return
-            grid[r][c] = '0'
-            for dr, dc in direction:
-                nr = r + dr
-                nc = c + dc
-                dfs(nr, nc)
-        count = 0
+
+        for i in range(n):
+            for j in range(m):
+
+                if grid[i][j] == '1':
+
+                    for dr, dc in dxn:
+                        nr = i  + dr
+                        nc = j  + dc
+                    
+                        if 0 <= nr < n and 0 <= nc < m and grid[nr][nc] == '1':
+                            union(i * m + j, nr * m + nc)
+        
+        answer = set()
+
+
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == '1':
-                    count += 1
-                    dfs(i, j)
-    
-        return count
+                    item = find(i*m + j)
+                    answer.add(item)
+        
+        return len(answer)
 
