3d252c11caac289ad3aa4045d96fa616532d3266
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-07-21 15:59:26 +0300

    Time: 1290 ms (52.95%) | Memory: 51.2 MB (23.07%) - LeetSync

--- 2176-parallel-courses-iii/parallel-courses-iii.py
+++ 2176-parallel-courses-iii/parallel-courses-iii.py
@@ -1,29 +1,23 @@
 class Solution:
-    def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-        adj = defaultdict(list)
-        indegree = [0] * n
-        
-        for u, v in relations:
-            adj[u-1].append(v-1)
-            indegree[v-1] += 1
-        
-        memo = {}
-        
-        def dp(node):
-            if node in memo:
+    def minimumTime(self, n: int, edges: List[List[int]], time: List[int]) -> int:
+            adj = defaultdict(list)
+
+            for u, v in edges:
+                adj[u-1].append(v-1)
+
+            memo = {}
+
+            def dp(node):
+                if node in memo:
+                    return memo[node]
+                res = time[node]
+
+                for next in adj[node]:
+                    res = max(res, time[node] + dp(next))
+                
+                memo[node] = res
                 return memo[node]
-            
-            max_time = time[node]
-            
-            for next_node in adj[node]:
-                max_time = max(max_time, dp(next_node) + time[node])
-            
-            memo[node] = max_time
-            return memo[node]
-        
-        result = 0
-        for i in range(n):
-            if indegree[i] == 0:
-                result = max(result, dp(i))
-        
-        return result
+            res = 0
+            for i in range(n):
+                    res = max(res, dp(i))
+            return res 
