c1ebec0fedf5c405e3585fbf56710efad035490b
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-09-06 19:43:24 +0300

    Time: 316 ms (13.94%) | Memory: 17.2 MB (18.19%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.py
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -9,10 +9,8 @@ class Solution:
         _max = 0
         for c in count:
             if c % 2 == 0:
-                if count[c] > _max:
+                if count[c] > _max or (count[c] == _max and c < ans):
                     _max = count[c]
                     ans = c
-        for c in count:
-            if count[c]  == _max  and c % 2 == 0:
-                ans = min(ans, c)
+    
         return ans
