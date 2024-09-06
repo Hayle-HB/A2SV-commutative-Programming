a4c4ee836420229a34f20c3d8c5583a278d0d118
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-09-06 18:10:10 +0300

    Time: 103 ms (31.18%) | Memory: 17.8 MB (97.57%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,11 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        count = Counter(nums)
-        n = len(nums)
-        ans = []
-        for c in count:
-            if count[c] > n // 3:
-                for j in range(count[c]):
-                    ans.append(c)
-
-        return list(set(ans)) 
+        if len(nums) <= 2:
+            return list(set(nums))
+        ans = set()
+        nums.sort()
+        curr = 1
+        [1, 2,2,3]
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                curr += 1
+            else:
+                if curr > len(nums) // 3:
+                    ans.add(nums[i-1])
+                curr = 1
+        
+        if curr > len(nums) // 3:
+            ans.add(nums[-1])
+        return list(ans)
