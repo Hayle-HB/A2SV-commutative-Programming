7aa4e56b63d85cbe267fa54d0aa54067b6ed5672
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-09-06 17:07:36 +0300

    Time: 179 ms (21.21%) | Memory: 18 MB (81.74%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = Counter(nums)
+
+        n = len(nums)
+
+        for c in count:
+            if count[c] > n // 2:
+                return c
+        
