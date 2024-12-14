c521be7f13e0cb566815bedb6780539dbd70144a
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-12-14 22:12:49 +0300

    Time: 51 ms (74.66%) | Memory: 37.6 MB (23.98%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,16 +1,17 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-
         prefix = {0:-1}
 
+
         curr = 0
+
         for right in range(len(nums)):
             curr += nums[right]
-            reminder = curr % k if k != 0 else curr
-            if reminder in prefix:
-                if right - prefix[reminder] > 1:
-                    return True
-            
-            if reminder not in prefix:
-                prefix[reminder] = right
-        return False
+
+            rem = curr % k if k != 0 else curr
+
+            if (rem in prefix )and (right - prefix[rem] > 1):
+                return True
+            if rem not in prefix:
+                prefix[rem] =right
+        return False 
