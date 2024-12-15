9447ab05a2431d79cb0ca6ab13e3e7fe07db74f7
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-12-15 15:26:59 +0300

    Time: 115 ms (74.19%) | Memory: 52.9 MB (69.35%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.py
@@ -0,0 +1,23 @@
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        n, dict1 = len(nums), defaultdict(list)
+
+        for i,v in enumerate(nums):
+            dict1[v].append(i)
+
+        arr = [0]*n 
+
+        for key,val in dict1.items():
+            pre_sum, suf_sum = 0, sum(val)
+            p, s = 0, len(val)
+
+            for i in val:
+                pre_sum += i
+                suf_sum -= i
+                p += 1
+                s -= 1
+                arr[i] = -pre_sum + i*p + suf_sum - i*s
+
+        return arr
+
+
