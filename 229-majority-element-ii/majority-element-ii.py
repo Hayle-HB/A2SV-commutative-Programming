80b1ebb587792e5d2170885b26f1f72f86e19d94
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-12-24 14:44:46 +0300

    Time: 5 ms (44.97%) | Memory: 19 MB (7.92%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,19 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        if len(nums) <= 2:
-            return list(set(nums))
-        ans = set()
-        nums.sort()
-        curr = 1
-        [1, 2,2,3]
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                curr += 1
-            else:
-                if curr > len(nums) // 3:
-                    ans.add(nums[i-1])
-                curr = 1
-        
-        if curr > len(nums) // 3:
-            ans.add(nums[-1])
-        return list(ans)
+        hash = defaultdict(int)
+        answer = set()
+        for num in nums:
+            hash[num] += 1
+
+            if hash[num] > len(nums) // 3 and num not in answer:
+                answer.add(num)
+        return list(answer)
