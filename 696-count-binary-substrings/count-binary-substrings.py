afa7302befb34e43e9db5c72092d213a4d7c414d
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-12-27 17:04:48 +0300

    Time: 71 ms (73.33%) | Memory: 18 MB (15.77%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.py
+++ 696-count-binary-substrings/count-binary-substrings.py
@@ -1,21 +1,11 @@
-class Solution:
-    def countBinarySubstrings(self, s: str) -> int:
-        arr = []
-        s = list(s)
-        count =   1
+class Solution(object):
+    def countBinarySubstrings(self, s):
+        ans, prev, curr = 0, 0, 1
+
         for i in range(1, len(s)):
-            if s[i] == s[i-1]:
-                count += 1
+            if s[i] != s[i-1]:
+                ans += min(prev, curr)
+                prev, curr = curr, 1
             else:
-                arr.append(count)
-                count = 1
-        arr.append(count)
-        answer = 0
-
-        for i in range(1, len(arr)):
-            answer += min(arr[i], arr[i-1])
-
-        return answer
-
-        
-        
+                curr += 1
+        return ans + min(prev, curr)
