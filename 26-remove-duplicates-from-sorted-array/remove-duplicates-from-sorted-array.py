7f10e5c7c6492ad23717af1eccdd1898b957bb87
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-12-27 16:14:17 +0300

    Time: 0 ms (100.00%) | Memory: 18.9 MB (11.25%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
             if nums[right] != nums[left]:
                 left += 1
                 nums[left] = nums[right]
-            right += 1
+
 
         return left+1
         
