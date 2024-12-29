dfb07be8fbc072f6b08bbda28819049295c3a24c
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-12-29 22:56:15 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (5.78%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,10 +1,22 @@
 class Solution(object):
     def isValid(self, s):
-        opcl = dict(('()', '[]', '{}'))
         stack = []
-        for idx in s:
-            if idx in '([{':
-                stack.append(idx)
-            elif len(stack) == 0 or idx != opcl[stack.pop()]:
-                return False
+        # Use dictionaries to map open and close brackets
+        open_to_close = {'(': ')', '{': '}', '[': ']'}
+        
+        for ch in s:
+            # If the character is an opening bracket, push to stack
+            if ch in open_to_close:
+                stack.append(ch)
+            else:
+                # If it's a closing bracket, check the stack
+                if not stack:
+                    return False
+                else:
+                    # Pop the top of the stack and check if it matches the closing bracket
+                    p = stack.pop()
+                    if open_to_close[p] != ch:
+                        return False
+        
+        # If the stack is empty at the end, the parentheses are balanced
         return len(stack) == 0
