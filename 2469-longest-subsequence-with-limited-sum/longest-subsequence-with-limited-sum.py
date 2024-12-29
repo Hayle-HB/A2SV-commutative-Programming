3eb6550a321a78682d804156dd027fd278786d94
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-12-29 19:44:13 +0300

    Time: 190 ms (13.08%) | Memory: 18.1 MB (12.33%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.py
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.py
@@ -1,15 +1,21 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         nums.sort()
-
-        curr = 0
-        answer = [0] * len(queries)
-        for i in range(len(nums)):
-            curr += nums[i]
-
-            for j in range(len(queries)):
-                if curr <= queries[j]:
-                    answer[j] = max(answer[j], i+1)
-        return answer
-            
+        prefix = [0] * len(nums)
+        prefix[0] = nums[0]
+        for i in range(1, len(nums)):
+            prefix[i] = prefix[i-1] + nums[i]
         
+
+        def bs(q):
+            left, right = 0, len(nums)-1
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                if prefix[mid] > q:
+                    right = mid - 1
+                else:
+                    left += 1
+            return left
+        return [bs(q) for q in queries]
